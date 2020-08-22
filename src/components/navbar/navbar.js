2caa1f87f75cf4d14dd73e6894b5a319314c15c0
Nicole Remy <nicoleremy95@gmail.com>
2020-08-22 12:13:13 -0700

    started working on responsive navbar

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./navbar.css"
+
+export default function Navbar() {
+    return (
+        <div className="menu-btn">
+            <div className ="neu-btn_burger"></div>
+        </div>
+    )
+}
