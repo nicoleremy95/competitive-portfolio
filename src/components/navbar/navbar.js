e49e818491ccd28ccc5c094be0fa2192829dc5f5
Nicole Remy <nicoleremy95@gmail.com>
2020-08-22 12:38:08 -0700

    menu hanburger css working

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Row, Col, Button } from 'antd';
+
 import "./navbar.css"
 
 export default function Navbar() {
+    const [menuBtn, setMenuBtn] = useState({
+        menuOpen: false,
+        menuClass: "menu-btn",
+        linksClass: "nav-links",
+    })
+
+    const handleMenuClick = () => {
+        if (!menuBtn.menuOpen) {
+            setMenuBtn({
+                menuOpen: !menuBtn.menuOpen,
+                menuClass: "menu-btn open"
+            })
+        } else {
+            setMenuBtn({
+                menuOpen: !menuBtn.menuOpen,
+                menuClass: "menu-btn"            
+            })
+        }
+    }
+
     return (
-        <div className="menu-btn">
-            <div className ="neu-btn_burger"></div>
+        <>
+        <div>
+            <Row >
+                <Col className={menuBtn.menuClass} onClick={handleMenuClick}>
+                    <div className="menu-btn_burger"></div>
+                </Col>
+            </Row>
+            {/* <Col className={menuBtn.linksClass}>
+                <Row justify="end">
+                    <Button type="text" >porfolio</Button> 
+                </Row>
+                <Row justify="end">
+                    <Button type="text" >contact me</Button>  
+                </Row>
+                <Row justify="end">
+                        <Button type="text" >about me</Button> 
+                </Row>
+            </Col> */}
         </div>
+        </>
     )
 }
