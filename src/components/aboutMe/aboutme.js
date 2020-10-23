b854429dc5f62b91611337446ffaa147c7023f3b
Nicole Remy <nicoleremy95@gmail.com>
2020-10-23 10:20:09 -0700

    cleaned up spacing, fonts, and alignment in about me section

--- src/components/aboutMe/aboutme.js
+++ src/components/aboutMe/aboutme.js
@@ -21,10 +21,8 @@ export default function Aboutme() {
                         </li>
                         <li>
                             <h2 
-                                
-                                className="text" 
                                 align="center"> 
-                                nremy@usc.edu
+                                <strong>nremy@usc.edu</strong>
                             </h2>
                         </li>
                         <li align="center">                
@@ -44,9 +42,9 @@ export default function Aboutme() {
                 span={14} 
                 offset={10} 
                 className="aboutme-text">
-                <p className="text" 
+                <p className="text-bio" 
                 align="justify"
-                >Full-stack web developer hoping to utilize a background in Architecture and Interior Design to create a more aesthetically pleasing design and user experience on the web. Worked with project managers to manage client relationships, projects, timelines, budgets, and customer satisfaction during time as an Architecture Associate. Recently graduated from University of Washington with a certificate in Full Stack Development. Previously graduated from the University of Southern California with a Bachelor of Science in GeoDesign. Excellent working with Github for deployment and collaboration and Heroku for deployment. Active problem solver with a passion for user experience and project management. Successfully worked with a team as a project manager on two projects: PLANiT and drinkME and in both instances finished the projects ahead of schedule and had the time and capacity to implement bonus features. I believe I will be an asset to any team to create more efficient, user friendly applications, within timelines and budget.</p>
+                ><strong>FULL STACK WEB DEVELOPER</strong> hoping to utilize a background in Architecture and Interior Design to create a more aesthetically pleasing design and user experience on the web. Worked with project managers to manage client relationships, projects, timelines, budgets, and customer satisfaction during time as an Architecture Associate. Recently graduated from University of Washington with a certificate in Full Stack Development. Previously graduated from the University of Southern California with a Bachelor of Science in GeoDesign. Excellent working with Github for deployment and collaboration and Heroku for deployment. Active problem solver with a passion for user experience and project management. Successfully worked with a team as a project manager on two projects: PLANiT and drinkME and in both instances finished the projects ahead of schedule and had the time and capacity to implement bonus features. I believe I will be an asset to any team to create more efficient, user friendly applications, within timelines and budget.</p>
             </Col>
             </Row>
             
