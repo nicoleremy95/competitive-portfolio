dd4acab2d5081793cb61ed7332b8a710fddd0aff
Nicole Remy <nicoleremy95@gmail.com>
2020-11-11 15:57:18 -0800

    added target blank to each link in portfolito section

--- src/components/portfolio_card/Portfolio_card.js
+++ src/components/portfolio_card/Portfolio_card.js
@@ -8,15 +8,15 @@ export default function PortfolioCard(props) {
     return (
       
     <div className="Portfolio-card">
-        <a className="Portfolio-name" href={props.app}>{props.name}</a>
+        <a className="Portfolio-name" href={props.app} target="_blank">{props.name}</a>
         <div className="Portfolio-image">
             <img src={props.img} alt={props.name}/>
         </div>
         <div className="Portfolio-role">{props.role}</div>
         <div className="Portfolio-tech">{props.tech}</div>
         <div className="Portfolio-description">{props.description}</div>
-        <a href={props.github}> <img className="Portfolio-icon"  src="https://img.icons8.com/fluent/48/000000/github.png"/></a>
-        <a href={props.app} > <img className="Portfolio-icon"   src="https://img.icons8.com/ios-filled/48/000000/application-window.png"/></a>
+        <a href={props.github} target="_blank"> <img className="Portfolio-icon"  src="https://img.icons8.com/fluent/48/000000/github.png"/></a>
+        <a href={props.app} target="_blank"> <img className="Portfolio-icon"   src="https://img.icons8.com/ios-filled/48/000000/application-window.png"/></a>
     </div>
      
     )
